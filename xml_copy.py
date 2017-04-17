f588053c729c180c99bbf08f7d403d4704e694ad
swkonstr <swkonstr@gmail.com>
2017-04-18 02:10:45 +0300

    Update xml_copy.py

--- xml_copy.py
+++ xml_copy.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import shutil
+import os.path
 
 tree = ET.parse('F:/OneDrive/Python/DrWeb_Test/file_list.xml')
 root = tree.getroot()
@@ -9,6 +10,9 @@ for files in root:
     name = files.get("name")
     input_path = files.find("input_path").text
     output_path = files.find("output_path").text
+    if os.path.exists(output_path) == 0:
+        os.mkdir(output_path)
+
     try:
         shutil.move(input_path+name, output_path+name)
         print("File "+name+" moved to "+output_path)
